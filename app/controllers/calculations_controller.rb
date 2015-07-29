b499554fa92a738dc16cb270354e07084b825473
Andi Fuess <afuess@chicagobooth.edu>
2015-07-29 12:00:00 -0500

    Commit_1

--- app/controllers/calculations_controller.rb
+++ app/controllers/calculations_controller.rb
@@ -2,4 +2,24 @@ class CalculationsController < ApplicationController
   def instructions
     render("instructions.html.erb")
   end
+  def loan_payment
+    render("loan_payment.html.erb")
+  end
+  def random
+    @min=params["min"]
+    @max=params["max"]
+    @rand=rand(@min.to_i..@max.to_i)
+    render("random.html.erb")
+  end
+  def square
+    @number=params["number"]
+    @number_squared=@number.to_i**2
+    render("square.html.erb")
+  end
+  def square_root
+    @number=params["number"]
+    @number_sqrt=Math.sqrt(@number.to_i)
+     render("square_root.html.erb")
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
   get("/", { :controller => "calculations", :action => "instructions" })
+  get("/square/:number", { :controller => "calculations", :action => "square" })
+  get("/square_root/:number", { :controller => "calculations", :action => "square_root" })
+  get("/random/:min/:max", { :controller => "calculations", :action => "random" })
+  get("/payment/INTEREST_RATE/NUMBER_OF_YEARS/PRINCIPAL_VALUE", { :controller => "calculations", :action => "loan_payment" })
+
+
+    # get("/flexible/:move", { :controller => "game", :action => "flexible" })
+
 end
